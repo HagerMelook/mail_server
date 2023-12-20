372c4c2dc111c0f52a191640955c6c4f8142a863
nouranKhalil <nouran.ahraf.youssef@gamil.com>
2023-12-20 02:35:41 +0200

    backend registeration

--- Backend/Backend/src/test/java/com/example/Backend/BackendApplicationTests.java
+++ Backend/Backend/src/test/java/com/example/Backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.Backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
