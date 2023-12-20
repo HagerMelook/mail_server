27969bec39b01e88a32234d2888ee7303a1f553a
HagerMelook <121068101+HagerMelook@users.noreply.github.com>
2023-12-20 16:39:44 +0200

    Update subjectFilter.java

--- Backend/Backend/src/main/java/com/example/Backend/subjectFilter.java
+++ Backend/Backend/src/main/java/com/example/Backend/subjectFilter.java
@@ -1,18 +1,1 @@
-package com.example.mailserver;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
-public class subjectFilter implements Filter{
-    private HashMap <String,ArrayList<Email>> subjectEmailFilter = new HashMap<String,ArrayList<Email>>();
-    @Override
-    public HashMap<String,ArrayList<Email>> emailFilter(ArrayList<Email> emails) {
-        for(Email email: emails){
-            if(!subjectEmailFilter.containsKey(email.getSubject())){
-                subjectEmailFilter.put(email.getSubject(),new ArrayList<Email>());
-            }
-            subjectEmailFilter.get(email.getSubject()).add(email);
-        }
-        return subjectEmailFilter;
-    }
-}
