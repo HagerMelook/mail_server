6bac05062ac9e88678c1654fce815cf729621d80
HagerMelook <121068101+HagerMelook@users.noreply.github.com>
2023-12-20 17:21:39 +0200

    Delete Backend/Backend/src/main/java/com/example/mailserver directory

--- Backend/Backend/src/main/java/com/example/mailserver/Filter.java
+++
@@ -1,9 +0,0 @@
-package com.example.mailserver;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-
-public interface Filter {
-
-    public HashMap<String,ArrayList<Email>> emailFilter(ArrayList<Email> emails);
-}
