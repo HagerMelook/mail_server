e57772ae1c711553a3f3f119525275482eb37faa
RanaMohamed6720 <121067602+RanaMohamed6720@users.noreply.github.com>
2023-12-25 21:20:46 +0200

    Update Request.java added emailType in order to know which section of the emails will be sorted

--- Backend/Backend/src/main/java/com/example/Backend/Request.java
+++ Backend/Backend/src/main/java/com/example/Backend/Request.java
@@ -4,13 +4,15 @@ package com.example.Backend;
 public class Request {
     private String userId ;
     private String sortingKeyword;
+    private String emailType;
 
     public Request() {
     }
 
-    public Request(String userId, String sortingKeyword) {
+    public Request(String userId, String sortingKeyword,String emailType) {
         this.userId = userId;
         this.sortingKeyword = sortingKeyword;
+        this.emailType=emailType;
     }
 
     public String getUserId() {
@@ -28,4 +30,14 @@ public class Request {
     public void setSortingKeyword(String sortingKeyword) {
         this.sortingKeyword = sortingKeyword;
     }
+
+	public String getEmailType() {
+		return emailType;
+	}
+
+	public void setEmailType(String emailType) {
+		this.emailType = emailType;
+	}
+    
 }
+
